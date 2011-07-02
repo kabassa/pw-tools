50f8d46075d1dbabb1c088991286594265adc4eb
max_schmerz@gmx.net <max_schmerz@gmx.net@2f73a061-7ea8-c242-2ef5-6e5f0b4166c8>
2011-07-02 09:08:56 +0000

    + added latest pwAdmin (08-2010) to binary release (without icons)

---
+++ trunk/bin/sPWadmin/jetty-7.4.2/webapps/pwAdmin/include/menu.js
@@ -0,0 +1,11 @@
+function show_submenu(id)
+{
+	//document.all[id].style.display = "block";
+	document.getElementById(id).style.visibility = "visible";
+}
+
+function hide_submenu(id)
+{
+	//document.all[id].style.display = "none";
+	document.getElementById(id).style.visibility = "hidden";
+}
