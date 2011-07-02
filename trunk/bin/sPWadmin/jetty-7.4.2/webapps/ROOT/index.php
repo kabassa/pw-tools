a9dd0834e369faff7a7809b5274a2355390664d5
max_schmerz@gmx.net <max_schmerz@gmx.net@2f73a061-7ea8-c242-2ef5-6e5f0b4166c8>
2011-07-02 08:47:51 +0000

    + added jetty 7.4.2 to pwAdmin binary release

---
+++ trunk/bin/sPWadmin/jetty-7.4.2/webapps/ROOT/index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+</head>
+
+<body>
+
+<div align="center">
+<br>
+<a href="pwAdmin/index.jsp"><b>pwAdmin</b></a>
+|
+<a href="adminer/adminer-3.2.2-mysql.php"><b>Adminer</b></a>
+</div>
+
+<br><br>
+
+<?
+	phpinfo();
+?>
+
+</body>
+
+</html>
