e2b2f4f36c36a3f6157908bed1696859a82a0588
max_schmerz@gmx.net <max_schmerz@gmx.net@2f73a061-7ea8-c242-2ef5-6e5f0b4166c8>
2011-07-11 12:10:16 +0000

    + sELedit: added rules for DE elements.data + sELedit: removed rules from client 1.4.4 to server + sELedit: renamed some rules + sELedit: added validation utilities for 1.3.6 client which helps to find invalid skills and properties

---
+++ trunk/sELedit/DebugWindow.h
@@ -0,0 +1,74 @@
+#pragma once
+
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+public ref class DebugWindow : public System::Windows::Forms::Form
+{
+	public: DebugWindow(void)
+	{
+		InitializeComponent();
+	}
+
+	public: DebugWindow(String^ Title, String^ Message)
+	{
+		InitializeComponent();
+		this->Text = Title;
+		this->message->Text = Message;
+		this->message->SelectionStart = 0;
+		this->message->SelectionLength = 0;
+		this->Show();
+	}
+
+	protected: ~DebugWindow()
+	{
+		if (components)
+		{
+			delete components;
+		}
+	}
+	private: System::Windows::Forms::TextBox^  message;
+
+	private: System::ComponentModel::Container ^components;
+
+	#pragma region Windows Form Designer generated code
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	void InitializeComponent(void)
+	{
+		this->message = (gcnew System::Windows::Forms::TextBox());
+		this->SuspendLayout();
+		// 
+		// message
+		// 
+		this->message->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+			| System::Windows::Forms::AnchorStyles::Left) 
+			| System::Windows::Forms::AnchorStyles::Right));
+		this->message->Location = System::Drawing::Point(0, 2);
+		this->message->Multiline = true;
+		this->message->Name = L"message";
+		this->message->ScrollBars = System::Windows::Forms::ScrollBars::Both;
+		this->message->Size = System::Drawing::Size(320, 478);
+		this->message->TabIndex = 0;
+		// 
+		// DebugWindow
+		// 
+		this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+		this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		this->ClientSize = System::Drawing::Size(320, 480);
+		this->Controls->Add(this->message);
+		this->Name = L"DebugWindow";
+		this->ShowIcon = false;
+		this->Text = L"DebugWindow";
+		this->ResumeLayout(false);
+		this->PerformLayout();
+
+	}
+	#pragma endregion
+};
---
+++ trunk/sPWadmin/jetty-start.bat
@@ -0,0 +1,2 @@
+CD jetty-7.4.2
+java -DSTOP.PORT=8000 -DSTOP.KEY=jetty_pid -jar start.jar OPTIONS=Server,jsp
