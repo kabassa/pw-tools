2c5d3d12618087cd7d2c29bd6e0a3c26b4e56a1a
max_schmerz <max_schmerz@gmx.net>
2012-02-28 16:31:50 +0000

    + sGSHOPedit: prepareations in interface and structure for switching to 1.4.2+ editing (1.3.6 will be abandoned soon) + sTASKedit: minor changes

--- COMMON/boutique.h
+++ COMMON/boutique.h
@@ -7,6 +7,18 @@ public ref struct Category
 	array<array<unsigned char>^>^ sub_cats; // Array [max 8] of 128 Byte Unicode
 };
 
+public ref struct SaleOption
+{
+	unsigned int price;
+	unsigned int expire_date;
+	unsigned int duration;
+	unsigned int start_date;
+	int type;
+	unsigned int day;
+	unsigned int status;
+	unsigned int flags;
+};
+
 public ref struct Item
 {
     int shop_id;
@@ -18,22 +30,15 @@ public ref struct Item
     int item_id;
     int item_amount;
 
-    int price1;
-	int value1;
-	int duration1;
-	int price2;
-	int value2;
-	int duration2;
-	int price3;
-	int value3;
-	int duration3;
+	array<SaleOption^>^ sale_options; // length = 4
 
-    int unknown1; // 16 Byte
-	int unknown2; // 16 Byte
-	int unknown3; // 16 Byte
-	int state; // 0->normal, 1->new, 2->hot, 4->gift
     array<unsigned char>^ description; // 1024 Byte Unicode
     array<unsigned char>^ name; // 64 Byte Unicode
+
+	int gift_id;
+	int gift_amount;
+	int gift_duration;
+	int log_price;
 };
 
 public ref struct Boutique
@@ -43,3 +48,30 @@ public ref struct Boutique
     array<Item^>^ items;
     array<Category^>^ categories; // [8]
 };
+
+/*
+type = class _GSHOP_ITEM
+{
+	public: int local_id;
+	int main_type;
+	int sub_type;
+	unsigned int id;
+	unsigned int num;
+	class
+	{
+		public: unsigned int price;
+		unsigned int end_time;
+		unsigned int time;
+		unsigned int start_time;
+		int type;
+		unsigned int day;
+		unsigned int status;
+		unsigned int flag;
+	}
+	buy[4];
+	unsigned int idGift;
+	unsigned int iGiftNum;
+	unsigned int iGiftTime;
+	unsigned int iLogPrice;
+} 
+*/
