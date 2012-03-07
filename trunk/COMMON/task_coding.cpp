41f0977481961386e211ae54343a3ad53d5d3b70
max_schmerz@gmx.net <max_schmerz@gmx.net@2f73a061-7ea8-c242-2ef5-6e5f0b4166c8>
2012-03-07 03:25:50 +0000

    + sTASKedit: added fields to interface + sTASKedit: updated structure + sTASKedit: added unicode <-> byte conversion

--- trunk/COMMON/task_coding.cpp
+++ trunk/COMMON/task_coding.cpp
@@ -94,3 +94,25 @@ array<unsigned char>^ GbkString_to_ByteArray(String^ text, int length)
 	}
 	return target;
 }
+
+String^ ByteArray_to_UnicodeString(array<unsigned char>^ text)
+{
+	Encoding^ enc = Encoding::GetEncoding("Unicode");
+	return enc->GetString(text)->Split(gcnew array<wchar_t>{'\0'})[0];
+}
+
+array<unsigned char>^ UnicodeString_to_ByteArray(String^ text, int length)
+{
+	Encoding^ enc = Encoding::GetEncoding("Unicode");
+	array<unsigned char>^ target = gcnew array<unsigned char>(length);
+	array<unsigned char>^ source = enc->GetBytes(text);
+	if(target->Length > source->Length)
+	{
+		Array::Copy(source, target, source->Length);
+	}
+	else
+	{
+		Array::Copy(source, target, target->Length);
+	}
+	return target;
+}
